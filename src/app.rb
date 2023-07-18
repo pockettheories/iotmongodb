b3b1918f0abd44c1b0e474f05458accc2b424afb
Nitin Reddy <82951937+nitredd@users.noreply.github.com>
2023-07-18 13:48:48 +0300

    Added output formatting

--- src/app.rb
+++ src/app.rb
@@ -1,5 +1,6 @@
 require 'mongo'
 require 'json'
+require 'cli/ui'
 
 mongouri = ENV['mongouri'] || 'mongodb://host.docker.internal'
 dbname = ENV['dbname'] || 'test'
@@ -10,6 +11,9 @@ sleeptime = (ENV['sleeptime'] || 1).to_i
 client = Mongo::Client.new mongouri
 client = client.use dbname
 
+CLI::UI.frame_style= :bracket
+CLI::UI::StdoutRouter.enable
+
 loop do
   val_to_insert = rand(30..50)
   doc = client[collname].find_one_and_update(
@@ -18,18 +22,19 @@ loop do
     {upsert: true, return_document: :after}
   )
 
-  puts "
-Executing:
+  CLI::UI::Frame.open("Record reading #{val_to_insert}") do
+    puts CLI::UI.fmt "{{green:Executing}}"
+    puts "
   db.#{collname}.updateOne(
     {meter: 'A1', reading_count: {$lt: bucketsize}},
     {$push: {'reading': #{val_to_insert}}, $inc: {reading_count: 1}},
     {upsert: true}
   );
-  "
-  puts "//Inserted #{val_to_insert}"
-  puts ''
-  puts 'Output document:'
-  puts JSON.pretty_generate(doc)
+    "
+    puts CLI::UI.fmt "{{green:Output document}}"
+    puts JSON.pretty_generate(doc)
+  end
+
   sleep sleeptime
 end
 
